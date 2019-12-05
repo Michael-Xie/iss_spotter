da6c91631c39c4685b6add3d626535b1a2c7d34a
Michael-Xie <xiemicha@gmail.com>
2019-12-05 21:05:30 +0000

    Added functionality to get long and lat based on ip

--- iss.js
+++ iss.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 const apiIP = "https://api.ipify.org?format=json";
-
+const apiGeo = "https://ipvigilante.com/";
 /**
  * Makes a single API request to retrieve the user's IP address.
  * Input:
@@ -29,4 +29,25 @@ const fetchMyIP = function(callback) {
     callback(null, data.ip);
   });
 };
-module.exports = { fetchMyIP };
+
+const fetchCoordsByIP = function(ip, callback) {
+  // use request to fetch IP address from JSON API
+  request(apiGeo+ip, (error, response, body) => {
+    // error can be set if invalid domain, user is offline, etc.
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    let data = JSON.parse(body);
+    callback(null, {latitude: data.data.latitude, longitude: data.data.longitude});
+  });
+
+}
+module.exports = { fetchMyIP, fetchCoordsByIP };
